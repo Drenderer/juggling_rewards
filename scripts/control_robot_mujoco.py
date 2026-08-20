90b12f37f06c4cfc915be6c0e151136e61f501d4
armin-sorme <armin.sormeabkenar@stud.tu-darmstadt.de>
2026-08-20 16:09:19 +0200

    add mujoco dynamics as model for mpc

--- scripts/control_robot_mujoco.py
+++ scripts/control_robot_mujoco.py
@@ -10,23 +10,26 @@ from pathlib import Path
 import mujoco as mj
 import numpy as np
 import time
-import imageio.v2 as imageio
-import imageio_ffmpeg
+
+
+import imageio
 
 from matplotlib.animation import FuncAnimation
 from IPython.display import HTML
-import copy
-from mpc_robot.datagen.mujoco_environment import MjEnvironment, MjViewer, Arm, Ball
+
+from mpc_robot.datagen.mujoco_environment import MjEnvironment, Arm
 from mpc_robot.datagen.normalize import Normalization
-from mpc_robot.datagen.main_with_policy import get_policy , get_viwer
+from mpc_robot.datagen.main_with_policy import get_policy ,get_viwer
 
 from mpc_robot.control.mpc_optax import mpc_optax
 from mpc_robot.control.mpc_optimistix import mpc_optimistix
+from mpc_robot.control.mpc_scipy import mpc_scipy
+
 from mpc_robot.models.sphnn import make_sphnn
 from mpc_robot.kinematic.forward import Forward_kinematic
 from mpc_robot.evaluation.evaluation_functions import  loss_history_mpc
 
-print(imageio_ffmpeg.get_ffmpeg_exe())
+
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ROOT = Path(__file__).resolve().parents[1]
@@ -68,7 +71,7 @@ norm = Normalization(mean_q=mean_q , alpha_q=alpha_q , tau_q=tau_q,
                      mean_u=mean_u , alpha_u=alpha_u)
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-DT = 0.002 #simulation time step
+DT = 0.002 
 t_rest = int(0.1/DT)
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 XML_PATH = PROJECT_ROOT / "robot_description" / "one_arm.xml"
@@ -84,6 +87,10 @@ def pd_control(y, q_des, dq_des, kp, kd):
     tau = kp * (q_des - q) + kd * (dq_des - dq)
     return  jnp.clip(tau, -MAX_CTRL, MAX_CTRL)
 
+
+
+
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Forward kinematic constant %%%%%%%%%%%%%%%%%
 L = jnp.array([-2.3360866e-05  ,-1.8214112e-05, 1.1860114e+00 ,5.4999387e-01 ,
                 2.5295885e-05 ,-4.5004494e-02 ,4.4200087e-01,2.5333025e-05 , 8.8627271e-02 ])
@@ -97,11 +104,33 @@ def calculation (L , q):
     robot_x = jnp.array([x , y , z])
     return robot_x
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% robot + pd + close loop %%%%%%%%%%%%%%%%%%%%%%%%
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% build mujoco model for dynamics of robot %%%%%%%
+def build_mujoco_model(XML_PATH):
 
-def true_control_step(y_true, q_des, dq_des):
-    tau = pd_control(y_true,q_des,dq_des, Kp , Kd)
-    return tau
+    mj_model = mj.MjModel.from_xml_path(str(XML_PATH))
+    data = mj.MjData(mj_model)
+
+    def model(ts, x0, u):
+        horizon = len(u)
+        mj.mj_resetData(mj_model, data)
+        data.qpos[:4] = np.asarray(x0[:4])
+        data.qvel[:4] = np.asarray(x0[4:])
+        mj.mj_forward(mj_model, data)
+        states = np.empty((horizon, 8))
+
+        for k in range(horizon):
+            data.ctrl[:4] = np.asarray(u[k])
+            mj.mj_step(mj_model, data)
+            states[k, :4] = data.qpos[:4]
+            states[k, 4:] = data.qvel[:4]
+
+        return states
+
+    return model
+
+mujoco_dynamics = build_mujoco_model(XML_PATH)
+
+
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% sphnn + pd + open loop %%%%%%%%%%%%%%%%%%%%%%%%
 @eqx.filter_jit
@@ -161,42 +190,43 @@ run_mpc2 = mpc_optimistix(
     u_max=MAX_CTRL,
     warm_start=True
 )
+
+run_mpc3 = mpc_scipy(
+    model = mujoco_dynamics,
+    horizon =20,
+    loss_fn=loss_function,
+    maxiter=10,
+    u_min=-MAX_CTRL,
+    u_max=MAX_CTRL,
+    warm_start=True
+)
+
+
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Mujoco simulation %%%%%%%%%%%%%%%%%%%%%%%%
 
-def main_mujoco(policy , q0 , dq0, motion_mode , input = None,T_end=1.0 , render =True ,name="mujoco_run.mp4"):
+def main_mujoco(policy , q0 , dq0, motion_mode , input = None,T_end=1.0 , render =True):
 
-    if motion_mode not in {"true" , "sphnn_pd_open" , "sphnn_mpc_open"}:
+    if motion_mode not in {"mujoco_pd_close" , "mujoco_mpc_open" , "sphnn_pd_open" , "sphnn_mpc_open"}:
         raise ValueError(
-            "motion_mode must be 'true' or 'sphnn_pd_open' ,''sphnn_mpc_open' "
+            "motion_mode must be 'mujoco_pd_close' ,'mujoco_mpc_open' or 'sphnn_pd_open' ,''sphnn_mpc_open' "
         )
     
-    if motion_mode in {"sphnn_pd_open" , "sphnn_mpc_open"}:
+    if motion_mode in {"Mujoco_mpc_open","sphnn_pd_open" , "sphnn_mpc_open"}:
         if input is None:
             raise ValueError(f"{motion_mode} requires input sequence")
     
     
     mj_model = mj.MjModel.from_xml_path(str(XML_PATH))     
-    data = mj.MjData(mj_model)  
+    data_simulation = mj.MjData(mj_model)  
+
     if render:
-        viewer = get_viwer(mj_model, data)
+        viewer = get_viwer(mj_model, data_simulation)
     else:
         viewer = None                             
                    
-    env = MjEnvironment(mj_model, data, viewer)  
-    arm = Arm(mj_model, data, "wam")    
-   
-    if name is not None:
-        width = mj_model.vis.global_.offwidth
-        height = mj_model.vis.global_.offheight
-        renderer = mj.Renderer(mj_model,height=height,width=width)
-        if viewer is not None:camera = copy.deepcopy(viewer.cam)
-        else:camera = None
-        writer = imageio.get_writer(name,fps=50,codec="libx264")
-    else:
-        renderer = None  
-        writer = None 
-        camera = None
-    
+    env = MjEnvironment(mj_model, data_simulation, viewer)  
+    arm = Arm(mj_model, data_simulation, "wam")    
+
     N = int(round(T_end / DT))
 
     q_hist  = np.empty((N+1, 4),dtype=jnp.float32 );  q_hist[0]  = np.array(q0)
@@ -206,19 +236,19 @@ def main_mujoco(policy , q0 , dq0, motion_mode , input = None,T_end=1.0 , render
     arm.q = np.asarray(q0, np.float64)
     arm.dq = np.asarray(dq0, np.float64)
     
-    mj.mj_forward(mj_model, data)   
-    skip = 10
+    mj.mj_forward(mj_model, data_simulation)   
+
     for k in range(N):
         qd, dqd = policy(k * DT)
         q_des  = jnp.asarray(qd,  jnp.float32)             
         dq_des = jnp.asarray(dqd, jnp.float32)    
 
-        if motion_mode == "true" :
+        if motion_mode == "mujoco_pd_close" :
             y_true = jnp.concatenate([
                     jnp.asarray(arm.q, dtype=jnp.float32),
                     jnp.asarray(arm.dq, dtype=jnp.float32),
                 ])
-            tau_np = true_control_step(y_true,q_des,dq_des)
+            tau_np = pd_control(y_true,q_des,dq_des, Kp , Kd)
 
 
         if motion_mode == "sphnn_pd_open":
@@ -228,6 +258,8 @@ def main_mujoco(policy , q0 , dq0, motion_mode , input = None,T_end=1.0 , render
         if motion_mode =="sphnn_mpc_open":
             tau_np = np.asarray(input[k],dtype=np.float64)
 
+        if motion_mode =="mujoco_mpc_open":
+            tau_np = np.asarray(input[k],dtype=np.float64)
 
         arm.tau = tau_np
         tau_hist[k] = tau_np
@@ -236,18 +268,11 @@ def main_mujoco(policy , q0 , dq0, motion_mode , input = None,T_end=1.0 , render
             env.step()
             env.render()
         else:
-            mj.mj_step(mj_model, data)
+            mj.mj_step(mj_model, data_simulation)
 
         q_hist[k+1] = arm.q
         dq_hist[k+1] = arm.dq
-        
-        if name is not None and k % skip == 0:
-            renderer.update_scene(data,camera=camera)
-            writer.append_data(renderer.render())
 
-    if writer is not None:
-        writer.close()
-        renderer.close()
     if viewer is not None:
         viewer.close()
     
@@ -261,9 +286,6 @@ q1 = jr.uniform(k1 , shape=(2,) , minval=-0.3 , maxval=0.3)
 q2 = jr.uniform(k2 , shape=(2,) , minval= 0.65 , maxval=1.45)
 q4 = jr.uniform(k3 , shape=(2,) , minval=0.65 , maxval=1.45)
 
-#q1 = jnp.array([0.3 , 0.3])
-#q2 = jnp.array([0.85 , 0.85])
-#q4 = jnp.array([0.75, 1.35])
 print (q1 , q2 , q4)
 policy = get_policy(q1  , q2 , q4 , 0.3)
 q0 , dq0 = policy(0.0)
@@ -281,41 +303,24 @@ for t in ts:
 q_policy = jnp.stack(q_policy)
 dq_policy = jnp.stack(dq_policy)
 
-
-
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% run mpc %%%%%%%%%%%%%%%%%%%%%%%%
 ts_norm = norm.transform_ts(ts)
 q_policy_norm = norm.transform_qs(q_policy)
 dq_policy_norm = norm.transform_q_ts(dq_policy)
 
-state_mpc_norm, tau_mpc_norm, loss_mpc , grad_mpc = run_mpc2(ts_norm,q_policy_norm,dq_policy_norm)
+state_sphnn_mpc_norm, tau_sphnn_mpc_norm, loss_sphnn_mpc , grad_sphnn_mpc = run_mpc2(ts_norm,q_policy_norm,dq_policy_norm)
+state_mujco_mpc, tau_mujoco_mpc, loss_mujoco_mpc = run_mpc3(ts,q_policy,dq_policy)
+tau_sphnn_mpc = norm.inverse_transform_taus(tau_sphnn_mpc_norm)
 
-tau_mpc = norm.inverse_transform_taus(tau_mpc_norm)
+#loss_history_mpc(loss_mujoco_mpc)
 
-loss_history_mpc(loss_mpc)
-
-plt.figure(figsize=(10, 5))
-
-plt.plot(grad_mpc[:, 0], label="Initial gradient norm")
-plt.plot(grad_mpc[:, -1], label="Final gradient norm")
-
-plt.yscale("log")
-plt.xlabel("MPC step")
-plt.ylabel("Gradient norm")
-plt.title("Initial and final MPC gradient norm")
-plt.grid()
-plt.legend()
-
-plt.show()
-
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% run pd %%%%%%%%%%%%%%%%%%%%%%%%
-state_pd, tau_pd = run_pd(sphnn_,q_policy,dq_policy)
+state_sphnn_pd, tau_sphnn_pd = run_pd(sphnn_,q_policy,dq_policy)
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% run simulation %%%%%%%%%%%%%%%%%
 
-q_hist_true, dq_hist_true, tau_hist_true = main_mujoco(policy, q0, dq0,motion_mode="true", T_end=1.0 , name="mujoco_run.mp4")
-#%%
+q_mujoco_pd, dq_mujoco_pd, tau_mujoco_pd = main_mujoco(policy, q0, dq0,motion_mode="mujoco_pd_close", T_end=1.0)
+
+
 '''
 ts_norm = norm.transform_ts(ts)
 q_hist_norm = norm.transform_qs(q_hist_true)
@@ -325,21 +330,28 @@ state_mpc_norm, tau_mpc_norm, loss_mpc = run_mpc(ts_norm,q_hist_norm,dq_hist_nor
 
 tau_mpc = norm.inverse_transform_taus(tau_mpc_norm)
 '''
-q_hist_sphnn, dq_hist_sphnn, tau_hist_sphnn = main_mujoco(policy, q0,dq0,motion_mode="sphnn_pd_open",input = tau_pd, T_end=1.0 , name=None)
-q_hist_mpc, dq_hist_mpc, tau_hist_mpc = main_mujoco( policy, q0, dq0,motion_mode="sphnn_mpc_open" ,input=tau_mpc, T_end=1.0 , name=None)
+q_sphnn_pd, dq_sphnn_pd, tau_sphnn_pd = main_mujoco(policy, q0,dq0,motion_mode="sphnn_pd_open",input = tau_sphnn_pd, T_end=1.0 )
+q_sphnn_mpc, dq_sphnn_mpc, tau_sphnn_mpc = main_mujoco(policy, q0,dq0,motion_mode="sphnn_mpc_open",input = tau_sphnn_mpc, T_end=1.0 )
+q_mujoco_mpc, dq_mujoco_mpc, tau_mujoco_mpc = main_mujoco( policy, q0, dq0,motion_mode="mujoco_mpc_open" ,input=tau_mujoco_mpc, T_end=1.0)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% evaluation in angles domain  %%%%%%%%%%%%%%%%%
+color_policy = 'red'
+color_mujoco_pd = 'blue'
+color_mujoco_mpc = 'green'
+color_sphnn_pd = 'purple'
+color_sphnn_mpc = 'orange'
 
 t_true = np.asarray(test_t)
 fig, axes = plt.subplots(2,2,figsize=(12, 8),sharex=True)
 axes = axes.ravel()
 
 for i, ax in enumerate(axes):
-    ax.plot(t_true[0],q_hist_true[:500, i],lw=1.6,label="True")
-    ax.plot(t_true[0],q_hist_sphnn[:500, i],lw=1.2,label="sphnn_pd")
-    ax.plot(t_true[0],q_hist_mpc[:500, i],lw=1.6,label="sphnn_mpc")
-    ax.plot(t_true[0],q_policy[:500, i],lw=1.6,label="policy")
+    ax.plot(t_true[0],q_policy[:500, i],lw=1.6,label="policy" , color = color_policy)
+    ax.plot(t_true[0],q_mujoco_pd[:500, i],lw=1.6,label="mujoco_pd" , color = color_mujoco_pd)
+    ax.plot(t_true[0],q_mujoco_mpc[:500, i],lw=1.6,label="mujoco_mpc" , color = color_mujoco_mpc)
+    ax.plot(t_true[0],q_sphnn_pd[:500, i],lw=1.2,label="sphnn_pd" , color = color_sphnn_pd)
+    ax.plot(t_true[0],q_sphnn_mpc[:500, i],lw=1.2,label="sphnn_mpc" , color = color_sphnn_mpc)
     ax.set_xlabel("t [s]")
     ax.set_ylabel(rf"$q_{i+1}$")
     ax.set_title(f"DoF {i+1} Position")
@@ -355,9 +367,11 @@ fig, axes = plt.subplots(2,2,figsize=(12, 8),sharex=True)
 axes = axes.ravel()
 
 for i, ax in enumerate(axes):
-    ax.plot(t_true[0],tau_hist_true[:500, i],lw=1.6,label="True")
-    ax.plot(t_true[0],tau_hist_sphnn[:500, i],lw=1.2,label="sphnn_pd")
-    ax.plot(t_true[0],tau_hist_mpc[:500, i],lw=1.6,label="sphnn_mpc")
+    ax.plot(t_true[0],tau_mujoco_pd[:500, i],lw=1.6,label="mujoco_pd" , color = color_mujoco_pd)
+    ax.plot(t_true[0],tau_mujoco_mpc[:500, i],lw=1.6,label="mujoco_mpc" , color =color_mujoco_mpc)
+    ax.plot(t_true[0],tau_sphnn_pd[:500, i],lw=1.2,label="sphnn_pd" , color = color_sphnn_pd)
+    ax.plot(t_true[0],tau_sphnn_mpc[:500, i],lw=1.2,label="sphnn_mpc" , color = color_sphnn_mpc)
+    
     ax.set_xlabel("t [s]")
     ax.set_ylabel(rf"$input_{i+1}$")
     ax.set_title(f"DoF {i+1} input")
@@ -374,12 +388,13 @@ plt.show()
 
 to_xyz = jax.jit(jax.vmap(lambda q: calculation(L, q)))
 
-xyz_true  = to_xyz(np.asarray(q_hist_true))
-xyz_sphnn = to_xyz(np.asarray(q_hist_sphnn))
-xyz_mpc   = to_xyz(np.asarray(q_hist_mpc))
+xyz_mujoco_pd  = to_xyz(np.asarray(q_mujoco_pd))
+xyz_sphnn_pd = to_xyz(np.asarray(q_sphnn_pd))
+xyz_sphnn_mpc   = to_xyz(np.asarray(q_sphnn_mpc))
+xyz_mujoco_mpc  = to_xyz(np.asarray(q_mujoco_mpc))
 xyz_policy = to_xyz(np.asarray(q_policy))
 
-print(xyz_true.shape)   # (N, 3)
+print(xyz_mujoco_pd.shape)   # (N, 3)
 
 
 fig, axes = plt.subplots(1, 3, figsize=(15, 4))
@@ -387,11 +402,12 @@ fig, axes = plt.subplots(1, 3, figsize=(15, 4))
 labels = ["x", "y", "z"]
 
 for i, ax in enumerate(axes):
-
-    ax.plot(ts[:len(xyz_true)], xyz_true[:, i], label="True")
-    ax.plot(ts[:len(xyz_sphnn)], xyz_sphnn[:, i], label="sphnn + PD")
-    ax.plot(ts[:len(xyz_mpc)], xyz_mpc[:, i], label="sphnn + MPC")
-    ax.plot(ts[:len(xyz_mpc)], xyz_policy[:, i], label="policy")
+    ax.plot(ts[:len(xyz_policy)], xyz_policy[:, i], label="policy" , color = color_policy)
+    ax.plot(ts[:len(xyz_mujoco_pd)], xyz_mujoco_pd[:, i], label="mujoco_pd" , color = color_mujoco_pd)
+    ax.plot(ts[:len(xyz_mujoco_mpc)], xyz_mujoco_mpc[:, i], label="mujoco_mpc" , color = color_mujoco_mpc)
+    ax.plot(ts[:len(xyz_sphnn_pd)], xyz_sphnn_pd[:, i], label="sphnn_pd" , color = color_sphnn_pd)
+    ax.plot(ts[:len(xyz_sphnn_mpc)], xyz_sphnn_mpc[:, i], label="sphnn_mpc" , color = color_sphnn_mpc)
+    
 
     ax.set_xlabel("Time [s]")
     ax.set_ylabel(f"{labels[i]} [m]")
@@ -401,94 +417,5 @@ for i, ax in enumerate(axes):
 
 plt.tight_layout()
 plt.show()
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% videos  %%%%%%%%%%%%%%%%%
 
-N = len(xyz_true)
-t = np.linspace(0, 1.0, N)
-
-fig = plt.figure(figsize=(8, 7))
-ax = fig.add_subplot(111, projection="3d")
-
-# fixed axis limits
-all_xyz = np.concatenate([xyz_true, xyz_sphnn, xyz_mpc ,xyz_policy], axis=0)
-
-ax.set_xlim(all_xyz[:, 0].min(), all_xyz[:, 0].max())
-ax.set_ylim(all_xyz[:, 1].min(), all_xyz[:, 1].max())
-ax.set_zlim(all_xyz[:, 2].min(), all_xyz[:, 2].max())
-ax.set_xlabel("x [m]")
-ax.set_ylabel("y [m]")
-ax.set_zlabel("z [m]")
-
-
-line_true,  = ax.plot([], [], [], label="True" , color ='blue')
-line_policy,  = ax.plot([], [], [], label="policy" , color ='red')
-line_sphnn, = ax.plot([], [], [], label="SPHNN + PD" , color ='orange',)
-line_mpc,   = ax.plot([], [], [], label="SPHNN + MPC" , color='green')
-
-point_true,  = ax.plot([], [], [], "o" , color ='blue')
-point_policy,  = ax.plot([], [], [], "o" , color ='red')
-point_sphnn, = ax.plot([], [], [], "o" , color ='orange')
-point_mpc,   = ax.plot([], [], [], "o" , color='green')
-
-ax.legend()
-
-title = ax.set_title("t = 0.000 s")
-
-
-def update(k):
-
-    # trajectories until current time
-    line_true.set_data(xyz_true[:k+1, 0],xyz_true[:k+1, 1])
-    line_true.set_3d_properties(xyz_true[:k+1, 2])
-
-    line_sphnn.set_data(xyz_sphnn[:k+1, 0],xyz_sphnn[:k+1, 1])
-    line_sphnn.set_3d_properties(xyz_sphnn[:k+1, 2])
-
-    line_mpc.set_data(xyz_mpc[:k+1, 0],xyz_mpc[:k+1, 1])
-    line_mpc.set_3d_properties(xyz_mpc[:k+1, 2])
-
-    line_policy.set_data(xyz_policy[:k+1, 0],xyz_policy[:k+1, 1])
-    line_policy.set_3d_properties(xyz_policy[:k+1, 2])
-
-    # moving end-effector points
-    point_true.set_data([xyz_true[k, 0]],[xyz_true[k, 1]])
-    point_true.set_3d_properties([xyz_true[k, 2]])
-
-    point_sphnn.set_data([xyz_sphnn[k, 0]],[xyz_sphnn[k, 1]])
-    point_sphnn.set_3d_properties([xyz_sphnn[k, 2]])
-
-    point_mpc.set_data([xyz_mpc[k, 0]],[xyz_mpc[k, 1]])
-    point_mpc.set_3d_properties([xyz_mpc[k, 2]])
-
-    point_policy.set_data([xyz_policy[k, 0]],[xyz_policy[k, 1]])
-    point_policy.set_3d_properties([xyz_policy[k, 2]])
-
-    title.set_text(f"t = {t[k]:.3f} s")
-
-    return (line_true,line_sphnn,line_mpc,line_policy,point_true,
-            point_sphnn,point_mpc,point_policy,title)
-
-skip = 10
-fps = 1 / (DT * skip)
-frames = np.arange(0, N, skip)
-
-ani = FuncAnimation(
-    fig,
-    update,
-    frames=frames,
-    interval=DT * skip * 1000,
-    blit=False
-)
-
-plt.close()
-
-HTML(ani.to_jshtml())
-
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% save videos  %%%%%%%%%%%%%%%%%
-ani.save(
-    "end_effector_trajectory.gif",
-    writer="pillow",
-    fps=20,
-    dpi=100
-)
 # %%
